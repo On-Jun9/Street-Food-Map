c8abeac9ab156241a893348e89ad66866a9b2aa1
HJ <u07032@daum.net>
2021-11-28 01:03:03 +0900

    Chore: Start develop
    Mybatis, 기본폴더 설정

---
+++ src/main/java/com/sfm/mapper/BoardMapper.java
@@ -0,0 +1,8 @@
+package com.sfm.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+}
---
+++ src/main/java/com/sfm/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.sfm.service;
+
+public interface BoardService {
+}
